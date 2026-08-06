8ac83abd7a99422ec93cfdb31441bb5a67c65df1
d0better <konomoses23@gmail.com>
2026-08-06 13:29:12 +0100

    array of objects and looping through it

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-let player1 = {
-    name: "knomic",
-    age:39,
-    club: "inter Miami"
-};
-let player2 = player1
-player2.name = "Messi"
-alert(player1.name)
+const team = {
+    name: "Barcelona",
+    players:["yamal", "raphina", "pedri"]
+}
+console.log(team.players[0])
