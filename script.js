e616f6b8147b95fb82912cb49adf59f12803f883
d0better <konomoses23@gmail.com>
2026-08-21 11:04:18 +0100

    usimg rest to determine the average number

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-function showPlayers(...players) {
-    console.log(players)
+function calculate(...numbers) {
+    return numbers.reduce((total, sum) => total + sum , 0) / numbers.length 
 }
-showPlayers("saka", "rice", "odgaard");
-function showPlayers(...players) {
-    for(let player of players){
-        console.log(player)
-    }
-}
+console.log(calculate(10, 20, 30));
+console.log(calculate(5, 10, 15, 20));
